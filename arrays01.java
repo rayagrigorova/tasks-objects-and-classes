531e836a1b320fb546231cff1195baf8f8ad72a0
raiagrigorova2 <72023155+raiagrigorova2@users.noreply.github.com>
2020-10-05 15:42:25 +0300

    Add files via upload

---
+++ arrays01.java
@@ -0,0 +1,26 @@
+package edu.smg;
+import java.util.*;
+public class arrays01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*		Създайте програма, която запълва масив с 20 произволни
+		елемента и извежда резултата в конзолата.*/
+		final int SZ = 20;
+		
+		int arr [] = new int[SZ];
+		
+		for(int i = 0; i < SZ; i++){
+			arr [i] = new Random().nextInt(2_147_483_646 + 1);
+			arr [i]	*= ((new Random().nextInt(1 + 1) + 1 == 1)? -1 : 1);
+		}
+		
+		System.out.println("Your array:");
+		
+		for(int i = 0; i < SZ; i++){
+			System.out.println(arr[i] + " ");
+		}
+		
+	}
+
+}
