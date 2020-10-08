9ada45905d31d99a4bfb70eae5e9f5d7b512abd5
raiagrigorova2 <72023155+raiagrigorova2@users.noreply.github.com>
2020-10-08 17:36:05 +0300

    Add files via upload

--- zad1_07.java
+++ zad1_07.java
@@ -1,6 +1,7 @@
 package edu.smg;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 import javax.swing.*;
 
@@ -9,7 +10,7 @@ public class zad1_07 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-/*		1 An anagram is a word or a phrase made by transposing
+/*		1 An anagram is a word or a phrase made by transposing     
 		the letters of another word or phrase; for example,
 		"parliament" is an anagram of "partial men," and "software"
 		is an anagram of "swear oft." Write a program that figures
@@ -31,11 +32,29 @@ public class zad1_07 {
 		String word_1 = txt_1.getText();
 		String word_2 = txt_2.getText();
 		
+		for(int i = 0; i < word_1.length();i++){
+			if(Pattern.matches("\\p{Punct}", String.valueOf(word_1.charAt(i)))){
+				String ch = String.valueOf(word_1.charAt(i));
+//				System.out.println(ch);
+				word_1 = word_1.replaceAll(ch, "");
+				
+			}
+		}
+		
+		for(int i = 0; i < word_2.length();i++){
+			if(Pattern.matches("\\p{Punct}", String.valueOf(word_2.charAt(i)))){
+				String ch = String.valueOf(word_2.charAt(i));
+				/*System.out.println(ch);*/
+				word_2 = word_2.replaceAll(ch, "");
+				
+			}
+		}
+		
 		word_1 = word_1.replaceAll("\\s", "");
 		word_2 = word_2.replaceAll("\\s", "");
 		
-		word_1.toLowerCase();
-		word_2.toLowerCase();
+		word_1 = word_1.toLowerCase();
+		word_2 = word_2.toLowerCase();
 		
 		
 		if(word_1.length()!=word_2.length()){JOptionPane.showMessageDialog(null, "The word is NOT an anagram");}
@@ -48,8 +67,8 @@ public class zad1_07 {
 			
 			for(int i=0;i<LEN;i++){
 				for(int j=0;j<LEN;j++){
-					if(word_1.charAt(i) == word_1.charAt(j) && mask[j] == false){
-						mask[j] = true;
+					if(word_1.charAt(i) == word_2.charAt(j) && mask[i] == false){
+						mask[i] = true;
 					}
 				}
 			}
